92522dc09d9ad90b68a6a84bdafa65f6bb1efecf
Tobias <tobiasbagg@googlemail.com>
2015-12-12 19:35:28 +0100

    [oscar-web] use album function of slimbox, add title to photos

--- oscar-web/www/public_html/js/libs/flickr/flickr.js
+++ oscar-web/www/public_html/js/libs/flickr/flickr.js
@@ -1,5 +1,5 @@
-define(["jqueryui", "slimbox"], function(){
-    return{
+define(["jqueryui", "slimbox"], function () {
+    return {
         api_key: "46f7a6dce46471b81aa7c1592fcc9733",
 
         query: function (text, geopos, callback) {
@@ -13,35 +13,38 @@ define(["jqueryui", "slimbox"], function(){
                 var photo;
                 for (var i in data.photos.photo) {
                     photo = data.photos.photo[i];
-                    urls.push('https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_t.jpg');
+                    urls.push({
+                        url: 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_t.jpg',
+                        title: photo.title
+                    });
                 }
                 callback(urls);
             });
         },
 
-        getImagesForLocation: function(name, geopos){
+        getImagesForLocation: function (name, geopos) {
             this.query(name, geopos, this._showImagesForLocation);
         },
 
-        _showImagesForLocation: function(urls){
+        _showImagesForLocation: function (urls) {
             var flickr = $('#flickr_images').empty();
 
             for (var i = 0; i < 50 && i < urls.length; i++) {
-                var url = urls[i];
-                var bigimg = url.replace("_t.jpg", "_b.jpg");
-                var link = $("<a />").attr("href", bigimg).attr("rel", "lightbox");
-                $("<img/>").attr("src", url).appendTo(link);
+                var obj = urls[i];
+                var bigimg = obj.url.replace("_t.jpg", "_b.jpg");
+                var link = $("<a />").attr("href", bigimg).attr("rel", "lightbox-group").attr("title", obj.title || "");
+                $("<img/>").attr("src", obj.url).appendTo(link);
                 link.appendTo(flickr);
             }
 
             if (urls.length > 0) {
-                $('#flickr').show("slide", { direction: "right" }, myConfig.styles.slide.speed);
+                $('#flickr').show("slide", {direction: "right"}, myConfig.styles.slide.speed);
                 // slimbox fails with dynamic content => updated added images
-                $("a[rel^='lightbox']").slimbox({}, null, function(el) {
+                $("a[rel^='lightbox']").slimbox({}, null, function (el) {
                     return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
                 });
             } else {
-                $('#flickr').hide("slide", { direction: "right" }, myConfig.styles.slide.speed);
+                $('#flickr').hide("slide", {direction: "right"}, myConfig.styles.slide.speed);
             }
         }
     }
