93898f01b818b5a922a0e5fefab8ac2933de44ed
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-03-14 13:11:22 +0100

    merge new query functions from upstream

--- oscar-web/www/public_html/js/libs/query/query.js
+++ oscar-web/www/public_html/js/libs/query/query.js
@@ -1,136 +1,113 @@
 define(["jquery", "state", "map", "conf"], function($, state, map, config){
     return query = {
-        clearGeoQueryMapShape: function () {
-            if (state.geoquery.mapshape !== undefined) {
-                state.map.removeLayer(state.geoquery.mapshape);
-                state.geoquery.mapshape = undefined;
+        function clearSpatialQueryMapShape() {
+            if (state.spatialquery.mapshape !== undefined) {
+                state.map.removeLayer(state.spatialquery.mapshape);
+                state.spatialquery.mapshape = undefined;
             }
-        },
-
-        clearGeoQuerySelect: function () {
-            query.endGeoQuerySelect();
-            $('#geoquery_acceptbutton').removeClass('btn-info');
-            $('#geoquery_selectbutton').html('Select rectangle');
-            state.geoquery.active = false;
-            query.clearGeoQueryMapShape();
-        },
-
-        updateGeoQueryMapShape: function () {
-            query.clearGeoQueryMapShape();
-            var sampleStep = 1.0 / config.geoquery.samplecount;
-            var sampleCount = config.geoquery.samplecount;
-            var pts = [];
-
-            function fillPts(sLat, sLng, tLat, tLng) {
-                var latDiff = (tLat - sLat) * sampleStep;
-                var lngDiff = (tLng - sLng) * sampleStep;
-                for (var i = 0; i < sampleCount; ++i) {
-                    pts.push(L.latLng(sLat, sLng));
-                    sLat += latDiff;
-                    sLng += lngDiff;
+        };
+        //begin spatial query functions
+        function spatialQueryOnClick(e) {
+            //reset timers
+            if (state.timers.spatialquery !== undefined) {
+                clearTimeout(state.timers.spatialquery);
+                state.timers.spatialquery = setTimeout(endSpatialQuery, myConfig.timeouts.spatialquery.select);
+            }
+            state.spatialquery.coords.push(e.latlng);
+            if (state.spatialquery.type === "rect") {
+                if (state.spatialquery.coords.length === 2) {
+                    endSpatialQuery();
                 }
             }
-
-            var minLat = parseFloat($('#geoquery_minlat').val());
-            var maxLat = parseFloat($('#geoquery_maxlat').val());
-            var minLng = parseFloat($('#geoquery_minlon').val());
-            var maxLng = parseFloat($('#geoquery_maxlon').val());
-
-            fillPts(minLat, minLng, minLat, maxLng);
-            fillPts(minLat, maxLng, maxLat, maxLng);
-            fillPts(maxLat, maxLng, maxLat, minLng);
-            fillPts(maxLat, minLng, minLat, minLng);
-
-            state.geoquery.mapshape = L.polygon(pts, config.styles.shapes.geoquery.normal);
-            state.map.addLayer(state.geoquery.mapshape);
-
-        },
-
-        geoQuerySelect: function (e) {
-            if (state.geoquery.clickcount === 0) {
-                $('#geoquery_minlat').val(e.latlng.lat);
-                $('#geoquery_minlon').val(e.latlng.lng);
-                $('[data-class="geoquery_min_coord"]').removeClass('bg-info');
-                $('[data-class="geoquery_max_coord"]').addClass('bg-info');
-                state.geoquery.clickcount = 1;
+            else {
+                this.updateSpatialQueryMapShape();
             }
-            else if (state.geoquery.clickcount === 1) {
-                $('#geoquery_maxlat').val(e.latlng.lat);
-                $('#geoquery_maxlon').val(e.latlng.lng);
-                state.geoquery.clickcount = 2;
-                query.endGeoQuerySelect();
+        };
+        function startSpatialQuery() {
+            if (state.timers.spatialquery !== undefined) {
+                clearTimeout(state.timers.spatialquery);
+                state.timers.spatialquery = undefined;
             }
-        },
+            $('#spatialquery_acceptbutton').removeClass("btn-info");
+            $('#spatialquery_selectbutton').addClass("btn-info");
+            $('#spatialquery_selectbutton').html("Finish");
+            state.spatialquery.selectButtonState = 'finish';
+            state.spatialquery.type = $('#spatialquery_type').val();
+            state.map.on('click', spatialQueryOnClick);
+            state.timers.spatialquery = setTimeout(endSpatialQuery, myConfig.timeouts.spatialquery.select);
+        };
+        function endSpatialQuery() {
+            this.updateSpatialQueryMapShape();
+            if (state.timers.spatialquery !== undefined) {
+                clearTimeout(state.timers.spatialquery);
+                state.timers.spatialquery = undefined;
+            }
+            state.map.removeEventListener('click', spatialQueryOnClick);
+            
+            state.spatialquery.selectButtonState = 'clear';
+            $('#spatialquery_selectbutton').html('Clear');
+            $('#spatialquery_acceptbutton').addClass('btn-info');
+        };
+        function clearSpatialQuery() {
+            if (state.timers.spatialquery !== undefined) {
+                clearTimeout(state.timers.spatialquery);
+                state.timers.spatialquery = undefined;
+            }
+            
+            this.clearSpatialQueryMapShape();
+            
+            state.map.removeEventListener('click', spatialQueryOnClick);
+            
+            state.spatialquery.coords = [];
+            state.spatialquery.selectButtonState = 'select';
+            state.spatialquery.type = undefined;
+            
+            $('#spatialquery_acceptbutton').removeClass("btn-info");
+            $('#spatialquery_selectbutton').removeClass("btn-info");
+            $('#spatialquery_selectbutton').html('Create');
+        };
+        function updateSpatialQueryMapShape() {
+            //clear old mapshape
+            this.clearSpatialQueryMapShape();
+            if (state.spatialquery.type === undefined) {
+                console.log("updateSpatialQueryMapShape called with undefined query type");
+                return;
+            }
+            else if (state.spatialquery.type === "rect") {
+                if (state.spatialquery.coords.length < 2) {
+                    return;
+                }
+                var sampleStep = 1.0 / config.geoquery.samplecount;
+                var sampleCount = config.geoquery.samplecount;
+                var pts = [];
+                function fillPts(sLat, sLng, tLat, tLng) {
+                    var latDiff = (tLat-sLat)*sampleStep;
+                    var lngDiff = (tLng-sLng)*sampleStep;
+                    for(i=0; i < sampleCount; ++i) {
+                        pts.push(L.latLng(sLat, sLng));
+                        sLat += latDiff;
+                        sLng += lngDiff;
+                    }
+                }
+                var minLat = Math.min(state.spatialquery.coords[0].lat, state.spatialquery.coords[1].lat);
+                var maxLat = Math.max(state.spatialquery.coords[0].lat, state.spatialquery.coords[1].lat);
+                var minLng = Math.min(state.spatialquery.coords[0].lng, state.spatialquery.coords[1].lng);
+                var maxLng = Math.max(state.spatialquery.coords[0].lng, state.spatialquery.coords[1].lng);
 
-        endGeoQuerySelect: function () {
-            if (state.timers.geoquery !== undefined) {
-                clearTimeout(state.timers.geoquery);
-                state.timers.geoquery = undefined;
-            }
-            $('#geoquery_selectbutton').removeClass("btn-info");
-            $('[data-class="geoquery_min_coord"]').removeClass('bg-info');
-            $('[data-class="geoquery_max_coord"]').removeClass('bg-info');
-            state.map.removeEventListener('click', map.geoQuerySelect);
-            $('#geoquery_acceptbutton').addClass('btn-info');
-            query.updateGeoQueryMapShape();
-        },
+                fillPts(minLat, minLng, minLat, maxLng);
+                fillPts(minLat, maxLng, maxLat, maxLng);
+                fillPts(maxLat, maxLng, maxLat, minLng);
+                fillPts(maxLat, minLng, minLat, minLng);
 
-        startGeoQuerySelect: function () {
-            if (state.timers.geoquery !== undefined) {
-                clearTimeout(state.timers.geoquery);
+                state.spatialquery.mapshape = L.polygon(pts, config.styles.shapes.geoquery.normal);
             }
-            $('#geoquery_acceptbutton').removeClass('btn-info');
-            state.geoquery.clickcount = 0;
-            state.geoquery.active = true;
-            $('#geoquery_selectbutton').addClass("btn-info").html('Clear rectangle');
-            $('[data-class="geoquery_min_coord"]').addClass('bg-info');
-            state.map.on('click', query.geoQuerySelect);
-            state.timers.geoquery = setTimeout(query.endGeoQuerySelect, config.timeouts.geoquery_select);
-        },
-
-        resetPathQuery: function () {
-            if (state.timers.pathquery !== undefined) {
-                clearTimeout(state.timers.pathquery);
+            else if (state.spatialquery.type === "poly") {
+                state.spatialquery.mapshape = L.polygon(state.spatialquery.coords, config.styles.shapes.polyquery.highlight);
             }
-            state.map.removeEventListener('click', query.pathQuerySelect);
-            state.map.removeLayer(state.pathquery.mapshape);
-            state.pathquery.mapshape = undefined;
-            state.pathquery.selectButtonState = 'select';
-            $('#pathquery_acceptbutton').removeClass("btn-info");
-            $('#pathquery_selectbutton').removeClass("btn-info").html('Select path');
-        },
-
-        pathQuerySelect: function (e) {
-            if (state.timers.pathquery !== undefined) {
-                clearTimeout(state.timers.pathquery);
-                state.timers.pathquery = setTimeout(query.endPathQuery, config.timeouts.pathquery.select);
+            else if (state.spatialquery.type === "path") {
+                state.spatialquery.mapshape = L.polyline(state.spatialquery.coords, config.styles.shapes.pathquery.highlight);
             }
-            if (state.pathquery.mapshape === undefined) {
-                state.pathquery.mapshape = L.polyline([], config.styles.shapes.pathquery.highlight).addTo(state.map);
-            }
-            state.pathquery.mapshape.addLatLng(e.latlng);
-        },
-
-        endPathQuery: function () {
-            if (state.timers.pathquery !== undefined) {
-                clearTimeout(state.timers.pathquery);
-            }
-            state.map.removeEventListener('click', query.pathQuerySelect);
-            $('#pathquery_acceptbutton').addClass("btn-info");
-            $('#pathquery_selectbutton').removeClass("btn-info").html('Clear path');
-            state.pathquery.selectButtonState = 'clear';
-        },
-
-        startPathQuery: function () {
-            if (state.timers.pathquery !== undefined) {
-                clearTimeout(state.timers.pathquery);
-            }
-            state.pathquery.points = []
-            $('#pathquery_acceptbutton').removeClass("btn-info");
-            $('#pathquery_selectbutton').addClass("btn-info").html("Finish path");
-            state.pathquery.selectButtonState = 'finish';
-            state.map.on('click', query.pathQuerySelect);
-            state.timers.pathquery = setTimeout(query.endPathQuery, config.timeouts.pathquery.select);
-        }
+            state.map.addLayer(state.spatialquery.mapshape);
+        };
     };
 });
