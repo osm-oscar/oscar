e63c212be8c76167d4c79f3aa00bb08e7b190b59
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-09-19 12:15:47 +0200

    [bin] stuff to support compilation of 32 Bit version on amd64

---
+++ bin/toolchain-clang32.cmake
@@ -0,0 +1,6 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# Which compilers to use for C and C++
+set(CMAKE_C_COMPILER ${CMAKE_SOURCE_DIR}/bin/i686-linux-gnu-clang)
+set(CMAKE_CXX_COMPILER ${CMAKE_SOURCE_DIR}/bin/i686-linux-gnu-clang++)
---
+++ bin/toolchain-gcc32.cmake
@@ -0,0 +1,7 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# Which compilers to use for C and C++
+set(CMAKE_C_COMPILER ${CMAKE_SOURCE_DIR}/bin/i686-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${CMAKE_SOURCE_DIR}/bin/i686-linux-gnu-g++)
+set(CMAKE_LINKER ${CMAKE_SOURCE_DIR}/bin/i686-ld)
