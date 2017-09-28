9f1cba4914eb313e0acabb4c40b166c8c6887df9
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-09-28 15:52:35 +0200

    [cmake] ultra buildtype

---
+++ cmake/add_buildtype_ultra.cmake
@@ -0,0 +1,45 @@
+cmake_minimum_required(VERSION 3.0)
+
+SET(CMAKE_CXX_FLAGS_ULTRA
+	"-DNDEBUG -O3 -march=native -flto -ffat-lto-objects -frounding-math"
+	CACHE STRING "Flags used by the C++ compiler during ultra builds."
+	FORCE
+)
+SET(CMAKE_C_FLAGS_ULTRA
+	"-DNDEBUG -O3 -march=native -flto -ffat-lto-objects -frounding-math"
+	CACHE STRING "Flags used by the C compiler during ultra builds."
+	FORCE
+)
+SET(CMAKE_EXE_LINKER_FLAGS_ULTRA
+	""
+	CACHE STRING "Flags used for linking binaries during ultra builds."
+	FORCE
+)
+SET(CMAKE_SHARED_LINKER_FLAGS_ULTRA
+	""
+	CACHE STRING "Flags used by the shared libraries linker during ultra builds."
+	FORCE
+)
+
+SET(LIBOSCAR_NO_DATA_REFCOUNTING_ENABLED
+	"True"
+	CACHE STRING "Disable data refcounting in liboscar"
+	FORCE
+)
+
+SET(SSERIALIZE_CONTIGUOUS_UBA_ONLY_SOFT_FAIL_ENABLED
+	"True"
+	CACHE STRING "Disable non-contiguous UBA"
+	FORCE
+)
+
+MARK_AS_ADVANCED(
+	CMAKE_CXX_FLAGS_ULTRA
+	CMAKE_C_FLAGS_ULTRA
+	CMAKE_EXE_LINKER_FLAGS_ULTRA
+	CMAKE_SHARED_LINKER_FLAGS_ULTRA
+)
+
+set(CMAKE_CONFIGURATION_TYPES  
+	"${CMAKE_CONFIGURATION_TYPES} ultra"
+)
