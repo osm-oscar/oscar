57688d46788eeef44dc5d899621b7e346a7e1d18
Daniel Bahrdt <daniel.git@funroll-loops.de>
2018-05-14 16:39:20 +0200

    [data] wip poi words

--- data/poi_words/extract.py
+++ data/poi_words/extract.py
@@ -4,14 +4,32 @@ import xml.dom.minidom
 import itertools
 import codecs
 import sys
+import enum
+#
+# {
+#	basePart : String
+#	relationPart: String
+#	spl : "singular" | "plural"
+#	rel : "none" | "in" | "near"
+# }
+
+class SPL(enum.Enum):
+	SINGULAR = "singular"
+	PLURAL = "plural"
+	def __str__(self):
+		return self.value
+
+class SpatialRelation(enum.Enum):
+	NONE = "none"
+	NEAR = "near"
+	IN = "in"
+	def __str__(self):
+		return self.value
 
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout) 
 
 cmdLineParser = argparse.ArgumentParser(description="Nix")
 cmdLineParser.add_argument('filename',  metavar='filename',  help="Name of the file to process",  type=str,  nargs='+')
-cmdLineParser.add_argument('-u', action='store_true',  dest='unique',  help='Unique output')
-cmdLineParser.add_argument('-w', action='store_true',  dest='unique',  help='First word until space')
-cmdLineParser.add_argument('-kv', action='store_true',  dest='kvonly',  help='Key=Values only')
 parsedCmdLine = cmdLineParser.parse_args()
 
 def getText(nodelist):
@@ -27,15 +45,62 @@ dom = xml.dom.minidom.parse(parsedCmdLine.filename[0])
 textElement = dom.getElementsByTagName('text')[0]
 lines = getText(textElement.childNodes).splitlines()
 
-outLines = []
+baseQS = dict() #String -> [SPL, tag_key, tag_value]
+spatialQS = [] #[String, SPL, SpatialRelation]
 
-for l in itertools.ifilter(lambda x: x.startswith("| "), lines):
+for l in filter(lambda x: x.startswith("| "), lines):
 	tokens = l[2:].split('||')
-	tokens = map(lambda x: x.strip(' '), tokens)
-	begin = 0
-	if (parsedCmdLine.kvonly):
-		begin = 1
-	outLines.append(tokens[begin:3])
-	myLine = []
-	for i in range(begin, 3):
-		myLinetokens[i]
+	tokens = list(map(lambda x: x.strip(' '), tokens))
+	qs = tokens[0]
+	tag_key = tokens[1]
+	tag_value = tokens[2]
+	rel = tokens[3]
+	spl = tokens[4]
+
+	if (spl == "Y"):
+		spl = SPL.PLURAL
+	else:
+		spl = SPL.SINGULAR
+
+	if (rel == "-"):
+		rel = SpatialRelation.NONE
+	elif (rel == "near"):
+		rel = SpatialRelation.NEAR
+	elif (rel == "in"):
+		rel = SpatialRelation.IN
+	else:
+		rel = SpatialRelation.NONE
+
+	if rel == SpatialRelation.NONE:
+		baseQS[qs] = [spl, tag_key, tag_value]
+		continue
+	
+	spatialQS.append([qs, spl, rel, tag_key, tag_value])
+
+print("{")
+print("\"base\": [")
+for qs, [spl, tag_key, tag_value] in baseQS.items():
+	print(str("{"))
+	print("\tbasePart: \"" + qs + "\"")
+	print("\trelationPart: \"\"")
+	print("\tspl: \"" + str(spl) + "\"")
+	print("\trel: \"none\"")
+	print("\ttag_key: \"" + tag_key + "\"")
+	print("\ttag_value: \"" + tag_value + "\"")
+	print("},")
+print("]")
+if False:
+	print("\"spatial\": [")
+	for qs, spl, rel, tag_key, tag_value in spatialQS:
+		basepart = qs
+		relpart = ""
+		print(str("{"))
+		print("\tbasePart: \"" + basepart + "\"")
+		print("\trelationPart: \"" + relpart + "\"")
+		print("\tspl: \"" + str(spl) + "\"")
+		print("\trel: \"" + str(rel) + "\"")
+		print("\ttag_key: \"" + tag_key + "\"")
+		print("\ttag_value: \"" + tag_value + "\"")
+		print("},")
+	print("]")
+print("}")
---
+++ data/poi_words/fetch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget 'https://wiki.openstreetmap.org/wiki/Special:Export/Nominatim/Special_Phrases/DE' -O de.xml
+wget 'https://wiki.openstreetmap.org/wiki/Special:Export/Nominatim/Special_Phrases/EN' -O en.xml
