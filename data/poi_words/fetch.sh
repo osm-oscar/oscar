5652510db5b2de9702de08733fd2a7fae1e536ae
Daniel Bahrdt <daniel.git@funroll-loops.de>
2018-05-15 14:23:09 +0200

    [data] remove stuff moved to oscar-web

--- data/poi_words/fetch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget 'https://wiki.openstreetmap.org/wiki/Special:Export/Nominatim/Special_Phrases/DE' -O de.xml
-wget 'https://wiki.openstreetmap.org/wiki/Special:Export/Nominatim/Special_Phrases/EN' -O en.xml
