39c76f02ad3c4e72c060d2afc7ae7c27fb4afbbd
Daniel Bahrdt <daniel.git@funroll-loops.de>
2018-06-26 13:00:13 +0200

    only add lto build type if it is not defined already

--- cmake/add_buildtype_lto.cmake
+++ cmake/add_buildtype_lto.cmake
@@ -4,34 +4,43 @@ IF(CMAKE_BUILD_TYPE MATCHES "^lto$")
 	include(setup-lto)
 ENDIF()
 
-SET(CMAKE_CXX_FLAGS_LTO
-	"-DNDEBUG -g -O3 -march=native -flto -fno-fat-lto-objects -frounding-math"
-	CACHE STRING "Flags used by the C++ compiler during lto builds."
-	FORCE
-)
-SET(CMAKE_C_FLAGS_LTO
-	"-DNDEBUG -g -O3 -march=native -flto -fno-fat-lto-objects -frounding-math"
-	CACHE STRING "Flags used by the C compiler during lto builds."
-	FORCE
-)
-SET(CMAKE_EXE_LINKER_FLAGS_LTO
-	""
-	CACHE STRING "Flags used for linking binaries during lto builds."
-	FORCE
-)
-SET(CMAKE_SHARED_LINKER_FLAGS_LTO
-	""
-	CACHE STRING "Flags used by the shared libraries linker during lto builds."
-	FORCE
-)
+IF ( (NOT LTO_BUILD_TYPE_ADDED) )
 
-MARK_AS_ADVANCED(
-	CMAKE_CXX_FLAGS_LTO
-	CMAKE_C_FLAGS_LTO
-	CMAKE_EXE_LINKER_FLAGS_LTO
-	CMAKE_SHARED_LINKER_FLAGS_LTO
-)
+	SET(CMAKE_CXX_FLAGS_LTO
+		"-DNDEBUG -g -O3 -march=native -flto -fno-fat-lto-objects -frounding-math"
+		CACHE STRING "Flags used by the C++ compiler during lto builds."
+		FORCE
+	)
+	SET(CMAKE_C_FLAGS_LTO
+		"-DNDEBUG -g -O3 -march=native -flto -fno-fat-lto-objects -frounding-math"
+		CACHE STRING "Flags used by the C compiler during lto builds."
+		FORCE
+	)
+	SET(CMAKE_EXE_LINKER_FLAGS_LTO
+		""
+		CACHE STRING "Flags used for linking binaries during lto builds."
+		FORCE
+	)
+	SET(CMAKE_SHARED_LINKER_FLAGS_LTO
+		""
+		CACHE STRING "Flags used by the shared libraries linker during lto builds."
+		FORCE
+	)
 
-set(CMAKE_CONFIGURATION_TYPES  
-	"${CMAKE_CONFIGURATION_TYPES} lto"
-)
+	MARK_AS_ADVANCED(
+		CMAKE_CXX_FLAGS_LTO
+		CMAKE_C_FLAGS_LTO
+		CMAKE_EXE_LINKER_FLAGS_LTO
+		CMAKE_SHARED_LINKER_FLAGS_LTO
+	)
+
+	SET(CMAKE_CONFIGURATION_TYPES  
+		"${CMAKE_CONFIGURATION_TYPES} lto"
+	)
+
+	SET(LTO_BUILD_TYPE_ADDED
+		"True"
+		CACHE INTERNAL ""
+		FORCE
+	)
+ENDIF()
