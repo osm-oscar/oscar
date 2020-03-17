7bf978c21c9908b7499f1813c19e1bc3cdcee8d0
Daniel Bahrdt <daniel.git@funroll-loops.de>
2020-03-17 02:17:44 +0100

    [cmake] improve lto setup

--- cmake/setup-lto.cmake
+++ cmake/setup-lto.cmake
@@ -43,31 +43,44 @@ IF ( (NOT LTO_PLUGIN_PARAMETERS_SET) OR (CMAKE_GCC_VERSION_FOR_LTO))
 			CACHE STRING ""
 			FORCE
 		)
-
-		find_path(CMAKE_LTO_PLUGIN_DIR
-			"liblto_plugin.so"
-			HINTS
-			"/usr/lib/gcc/x86_64-linux-gnu/${CMAKE_GCC_VERSION_FOR_LTO}"
-			"/usr/libexec/gcc/x86_64-pc-linux-gnu/${CMAKE_GCC_VERSION_FOR_LTO}"
-		)
-		SET(CMAKE_LTO_PLUGIN_PATH
-			"${CMAKE_LTO_PLUGIN_DIR}/liblto_plugin.so"
-			CACHE STRING "path to gcc liblto plugin"
-			FORCE
-		)
-
-		SET(CMAKE_C_ARCHIVE_CREATE
-			"<CMAKE_AR> rcs --plugin ${CMAKE_LTO_PLUGIN_PATH} <TARGET> <OBJECTS>"
-			CACHE INTERNAL ""
-			FORCE
-		)
-
-		SET(CMAKE_CXX_ARCHIVE_CREATE
-			"<CMAKE_AR> rcs --plugin ${CMAKE_LTO_PLUGIN_PATH} <TARGET> <OBJECTS>"
-			CACHE INTERNAL ""
-			FORCE
-		)
+		
+		if (NOT EXISTS "${CMAKE_LTO_PLUGIN_PATH}")
+			message(STATUS "Trying to find lto plugin path to set CMAKE_LTO_PLUGIN_PATH")
+			find_path(CMAKE_LTO_PLUGIN_DIR
+				"liblto_plugin.so"
+				"liblto_plugin.so.0"
+				"liblto_plugin.so.0.0"
+				"liblto_plugin.so.0.0.0"
+				"liblto_plugin.so.${CMAKE_GCC_VERSION_FOR_LTO}"
+				HINTS
+				"/usr/lib/gcc/x86_64-linux-gnu/${CMAKE_GCC_VERSION_FOR_LTO}"
+				"/usr/lib/gcc/x86_64-pc-linux-gnu/${CMAKE_GCC_VERSION_FOR_LTO}"
+				"/usr/libexec/gcc/x86_64-pc-linux-gnu/${CMAKE_GCC_VERSION_FOR_LTO}"
+			)
+			SET(CMAKE_LTO_PLUGIN_PATH
+				"${CMAKE_LTO_PLUGIN_DIR}/liblto_plugin.so"
+				CACHE STRING "path to gcc liblto plugin"
+				FORCE
+			)
+        endif()
+		if(NOT EXISTS "${CMAKE_LTO_PLUGIN_PATH}")
+            message(FATAL_ERROR "CMAKE_LTO_PLUGIN_PATH not set")
+		else()
+			message(STATUS "CMAKE_LTO_PLUGIN_PATH: ${CMAKE_LTO_PLUGIN_PATH}")
+		endif()
 	endif()
+	
+	SET(CMAKE_C_ARCHIVE_CREATE
+		"<CMAKE_AR> rcs --plugin ${CMAKE_LTO_PLUGIN_PATH} <TARGET> <OBJECTS>"
+		CACHE INTERNAL ""
+		FORCE
+	)
+
+	SET(CMAKE_CXX_ARCHIVE_CREATE
+		"<CMAKE_AR> rcs --plugin ${CMAKE_LTO_PLUGIN_PATH} <TARGET> <OBJECTS>"
+		CACHE INTERNAL ""
+		FORCE
+	)
 	
 	SET(LTO_PLUGIN_PARAMETERS_SET
 		"True"
